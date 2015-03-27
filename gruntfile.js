340d53df0a8faca89385fde887a28f9cbd4500d9
WhatsNewSaes <scoelen89@gmail.com>
2015-03-27 16:11:54 -0400

    Adds initial files

---
+++ gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+// grunt plugins configurations here
+
+    	    });
+
+// load NPM tasks in here
+// eg: grunt.loadNpmTasks('grunt-contrib-less');
+
+};
